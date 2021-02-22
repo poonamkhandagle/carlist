5933fa53150409e026293d1339e3e4c4566f44c8
poonam-khandagle <poonam@kudosfinance.in>
2021-02-22 15:54:23 +0530

    api folder deleted

--- src/api/unsplash.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-export default axios.create({
-  baseURL:"https://api.unsplash.com",
-  headers: {
-    Authorization: "Client-ID i3e2tK5ViUer-i4wJIepzGAFN2LiKSewkUgu0xGKBy0 "
-  }
-})
